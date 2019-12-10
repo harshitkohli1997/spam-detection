0213ffe0f7dd021fc27e8c10b1c05847e0a77d43
harshit.kohli1997@gmail.com <harshit.kohli1997@gmail.com>
2019-12-11 02:49:19 +0530

    fix bug

--- spam-detect.js
+++ spam-detect.js
@@ -1,25 +1,16 @@
 const BayesClassifier = require('bayes-classifier');
 
-const data = require('./dataset/dataset');
-
 var classifier = new BayesClassifier()
 var storedClassifier = require('./store.json');
 classifier.restore(storedClassifier)
 
-// for(var k =0; k < 20; k++)
-// {
-//     console.log(classifier.classify(data[k].input),' actual ans is '+data[k].output);
-// }
-
 module.exports.detect = function(str) {
    var ans =  classifier.classify(str);
-   
    return ans;
 }
 
 module.exports.getResults = function(str){
    var ans = classifier.getClassifications(str);
-
    return ans;
 }
 
